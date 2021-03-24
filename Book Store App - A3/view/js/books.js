80643a9c664a138fef1f791fc040c816ed3d40a8
yeeteing <ytlo@mun.ca>
2021-03-24 13:40:08 -0230

    listen to event for all the buttons

---
+++ Book Store App - A3/view/js/books.js
@@ -0,0 +1,42 @@
+$(document).ready(function () {
+    /**
+     * If add a book button is clicked
+     * show a form
+     */
+    $("#add-a-book").click(function (event) {
+        console.log('x')
+    });
+
+    /**
+     * If show books button is clicked
+     * show a form
+     */
+    $("#show-books").click(function (event) {
+        console.log('1')
+    });
+
+    /**
+     * If detail book button is clicked
+     * show a form
+     */
+    $("#detail-book").click(function (event) {
+        console.log('4')
+    });
+
+    /**
+     * If update book button is clicked
+     * show a form
+     */
+    $("#update-book").click(function (event) {
+        console.log('2')
+    });
+
+    /**
+     * If delete book button is clicked
+     * show a form
+     */
+    $("#delete-book").click(function (event) {
+        console.log('x')
+    });
+
+});
---
+++ Book Store App - A3/view/js/loans.js
@@ -0,0 +1,34 @@
+$(document).ready(function () {
+    /**
+     * If add a loa button is clicked
+     * show a form
+     */
+    $("#add-a-loan").click(function (event) {
+        console.log('l')
+    });
+
+    /**
+     * If show loans button is clicked
+     * show a form
+     */
+    $("#show-loans").click(function (event) {
+        console.log('ls')
+    });
+
+    /**
+     * If update loan button is clicked
+     * show a form
+     */
+    $("#update-loan").click(function (event) {
+        console.log('i')
+    });
+
+    /**
+     * If delete loan button is clicked
+     * show a form
+     */
+    $("#delete-loan").click(function (event) {
+        console.log('sx')
+    });
+
+});
