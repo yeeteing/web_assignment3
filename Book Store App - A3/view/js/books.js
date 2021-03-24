4cd8ff5aac5dad0ccdf0891656f92d05230831ee
yeeteing <ytlo@mun.ca>
2021-03-24 21:24:12 -0230

    update nav css

--- Book Store App - A3/view/js/books.js
+++ Book Store App - A3/view/js/books.js
@@ -1,12 +1,29 @@
 
 $(document).ready(function () {
+
     $(".book-detail, .event-response").each(function (index) {
         $(this).hide();
+    });
+
+    $(".bookNav, .tablinks, .loanNav").hover(function () {
+        $(this).css("background-color", "#f0e0e8");
+    }, function () {
+        $(this).css("background-color", "#f1f1f1");
+    })
+
+    $(".tablinks").click(function (index) {
+        // get the clicked id
+        let this_id = $(this).attr('id');
+        $("button").removeClass("activeTab")
+        $(this).addClass("activeTab")
     });
 
     $(".bookNav").click(function (index) {
         // get the clicked id
         let this_id = $(this).attr('id');
+        $("button").removeClass("activeBook")
+        $(this).addClass("activeBook")
+        
         changeModifyContents(this_id);
 
         $(".event-response").each(function (index) {
@@ -14,10 +31,11 @@ $(document).ready(function () {
         });
     });
 
-    function changeModifyContents(modifyContnet) {
+    function changeModifyContents(clickedId) {
+
         $(".book-detail").each(function () {
             let this_id = $(this).attr('id');
-            if (this_id.includes(modifyContnet.toLowerCase())) {
+            if (this_id.includes(clickedId.toLowerCase())) {
                 $(this).show();
             } else {
                 $(this).hide();
--- Book Store App - A3/view/js/loans.js
+++ Book Store App - A3/view/js/loans.js
@@ -6,6 +6,8 @@ $(document).ready(function () {
     $(".loanNav").click(function (index) {
         // get the clicked id
         let this_id = $(this).attr('id');
+        $("button").removeClass("activeLoan")
+        $(this).addClass("activeLoan")
         changeModifyContents(this_id);
 
         $(".event-response").each(function (index) {
