69aefc0a0fcf51864f303ca39ac4cc48bb7f004c
yeeteing <ytlo@mun.ca>
2021-03-24 17:12:40 -0230

    update change for class and styling in book

--- Book Store App - A3/view/js/books.js
+++ Book Store App - A3/view/js/books.js
@@ -1,10 +1,10 @@
 
 $(document).ready(function () {
-    $(".modify, .book-detail, .event-response").each(function (index) {
+    $(".book-detail, .event-response").each(function (index) {
         $(this).hide();
     });
 
-    $(".modifyBookNav").click(function (index) {
+    $(".bookNav").click(function (index) {
         // get the clicked id
         let this_id = $(this).attr('id');
         changeModifyContents(this_id);
@@ -25,20 +25,8 @@ $(document).ready(function () {
     });
 
     function changeModifyContents(modifyContnet) {
-        $(".modify").each(function () {
-            let this_id = $(this).attr('id');
-            if (this_id.includes(modifyContnet.toLowerCase())) {
-                $(this).show();
-            } else {
-                $(this).hide();
-            }
-        });
-    }
-
-    function changeDetailContents(modifyContnet) {
         $(".book-detail").each(function () {
             let this_id = $(this).attr('id');
-            console.log(this_id)
             if (this_id.includes(modifyContnet.toLowerCase())) {
                 $(this).show();
             } else {
