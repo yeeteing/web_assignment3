849b1b5b826125785c61f23bfd86d67afa5fc130
yeeteing <ytlo@mun.ca>
2021-03-24 13:27:49 -0230

    allow content changes if click books/loans button

--- Book Store App - A3/view/js/main.js
+++ Book Store App - A3/view/js/main.js
@@ -1,20 +1,29 @@
 /**
- * This script will add the main behavior of the tabs.
- * The idea is that every time a tab is clicked, we identify the one that was clicked,
- * call show to this one, and hide to the others.
+ * Run this scripts on loan
+ * When dom is ready, hide two books and loans content.
+ * When one if (books or loans) is clicked, show the content
  */
-$(document).ready(function(){
-    function changeDivComponents(tab_element_clicked){
-        $(".tabcontent").each(function(index){
+$(document).ready(function () {
+    // hide the books and loans content while DOM ready
+    $(".content").each(function (index) {
+        $(this).hide();
+    });
+
+    // if any of the link is clicked, shows the content
+    $(".tablinks").click(function () {
+        changeContents($(this).text());
+    });
+
+    // function for switching contents
+    function changeContents(tab_element_clicked) {
+        $(".content").each(function (index) {
             let this_id = $(this).attr('id');
-            if (this_id.includes(tab_element_clicked.toLowerCase())){
+            if (this_id.includes(tab_element_clicked.toLowerCase())) {
                 $(this).show();
-            }else{
+            } else {
                 $(this).hide();
-            }                    
+            }
         });
     }
-    $(".tablinks").click(function(){
-        changeDivComponents($(this).text());
-    });
+
 });
